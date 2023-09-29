71e02f133a5126ff7205200a8682fbf9c3b2f772
thanuja <thanuja@LAPTOP-PPLRC5HS>
2023-09-29 15:00:37 +0530

    Second Commit

---
+++ src/Continue.java
@@ -0,0 +1,17 @@
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int k =1; k<15; k++)
+		{
+			if (k%2 !=0)
+				continue;
+			System.out.println(k + " ");
+			
+		}
+
+	}
+
+}
